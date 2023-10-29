ae3ea94b3435a5eee6bca67c43cc31397b0432ca
unknown <alberdsp@gmail.com>
2023-10-29 17:47:28 +0100

    creado botón para cambiar y volver de activity

--- app/src/main/java/com/example/myappfragments/MainActivity2.java
+++ app/src/main/java/com/example/myappfragments/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.myappfragments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button btnVolver = findViewById(R.id.btnVolver);
+        btnVolver.setOnClickListener(iractivity1);
+
+
+
+
     }
+
+    private View.OnClickListener iractivity1 = new View.OnClickListener() {
+
+        @Override
+
+        public void onClick(View view){
+
+            Intent intent = new Intent(MainActivity2.this,
+                    MainActivity.class);
+
+            startActivity(intent);
+        }
+
+
+
+
+    };
+
+
+
+
+
 }
